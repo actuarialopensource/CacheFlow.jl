be34259eca98dba5f200e2a55cdbdeeb6c7f116e
serenity4 <cedric.bel@hotmail.fr>
2023-04-16 21:07:43 +0200

    Use Memoize.jl for caching, reimplement dot products with fixed final timestep

--- src/gpt.jl
+++ src/gpt.jl
@@ -1,5 +1,5 @@
 module Gpt
-using DataFrames, CSV, LinearAlgebra, Statistics, Folds, Memoize
+using DataFrames, CSV, LinearAlgebra, Statistics, Memoize
 
 mort_df = Matrix{Float64}(CSV.read(joinpath(@__DIR__, "models", "mort_table.csv"), DataFrame)[:, 2:end])
 const model_points = CSV.read(joinpath(@__DIR__, "models", "model_point_table.csv"), DataFrame)
@@ -8,6 +8,8 @@ Base.@kwdef struct BasicMortality
     rates::Matrix{Float64} = Matrix{Float64}(CSV.read(joinpath(@__DIR__, "models", "mort_table.csv"), DataFrame)[:, 2:end])
     issue_age::Vector{Int} = model_points[:, :age_at_entry]
 end
+
+const final_timestep = Ref(500)
 
 duration(t::Int) = t ÷ 12
 
@@ -18,16 +20,14 @@ monthly_basic_mortality(t) = get_monthly_rate(basic_mortality, t)
 policies_death(t) = policies_inforce(t) .* monthly_basic_mortality(t)
 
 # policies_inforce(t) = t == 0 ? ones(length(issue_age)) : (t > 12policies_term() ? 0 : policies_inforce(t - 1) .- policy_lapses(t - 1) .- policies_death(t - 1) .- policies_maturity(t))
-d = Dict()
-function policies_inforce(t)
-  key = (policies_inforce, t)
-  get!(d, key) do
-    t == 0 && return ones(length(issue_age))
-    policies_inforce(t - 1) .- policies_lapse(t - 1) .- policies_death(t - 1) .- policies_maturity(t)
-  end
+const cache_policies_inforce = Dict{Tuple{Int64},Any}()
+@memoize Returns(cache_policies_inforce)() function policies_inforce(t)
+  t == 0 && return ones(length(issue_age))
+  policies_inforce(t - 1) .- policies_lapse(t - 1) .- policies_death(t - 1) .- policies_maturity(t)
 end
 policies_lapse(t) = (policies_inforce(t) .- policies_death(t)) .* (1 - (1 - lapse_rate(t))^(1/12))
 lapse_rate(t) = max(0.1 - 0.02 * duration(t), 0.02)
+
 policies_term() = model_points[:, :policy_term]
 
 function policies_maturity(t)
@@ -48,30 +48,27 @@ age_at_entry() = model_points[:, :age_at_entry]
 claim_pp(t::Int) = sum_assured #unused
 claims(t::Int) = claim_pp(t) .* policies_death(t)
 commissions(t::Int) = duration(t) == 0 ? premiums(t) : 0
-disc_factors() = [(1 + disc_rate_mth(t))^(-t) for t in 0:12*policies_term]
+disc_factors() = [(1 + disc_rate_mth(t))^(-t) for t in final_timestep[]]
 disc_rate_mth(t::Int)::Float64 = (1 + disc_rate_ann(duration(t)))^(1/12) - 1
 disc_rate_ann(t::Int)::Float64 = 0.05 # where does that come from? t is unused
 expenses(t::Int) = t == 0 ? expense_acq .+ (policies_inforce(t) .* (expense_maint / 12) .* inflation_factor(t)) : (policies_inforce(t) .* (expense_maint / 12) .* inflation_factor(t))
 inflation_factor(t::Int) = (1 .+ inflation_rate).^(t/12)
 # this is tested but performance concerns.
 disc_factor(t) = (1 + zero_spot[duration(t)+1])^(-t/12)
-pv_claims() = Folds.sum(t -> claims(t) * disc_factor(t), 0:500)
+pv_claims() = sum(t -> claims(t) * disc_factor(t), 0:500)
+pv_commissions() = sum(t -> commissions(t) * disc_factor(t), 0:final_timestep[])
+pv_expenses() = sum(t -> expenses(t) * disc_factor(t), 0:final_timestep[])
+pv_pols_if() = sum(t -> policies_inforce(t) * disc_factor(t), 0:final_timestep[])
+pv_premiums() = sum(t -> premiums(t) * disc_factor(t), 0:final_timestep[])
 
 # See that net_premium_pp() references pv_claims, an expensive function. This gets called for every t during premiums(t::Int).
 net_premium_pp() = pv_claims() ./ pv_pols_if()
-premium_pp() = round.((1 .+ loading_prem) .* net_premium_pp(), 2)
+const cache_premiums_pp = Dict{Tuple{},Any}()
+@memoize Returns(cache_premiums_pp)() premium_pp() = round.((1 .+ loading_prem) .* net_premium_pp(); digits = 2)
 premiums(t::Int) = premium_pp() .* policies_inforce(t)
-pv_commissions() = dot(commissions.(0:12*policies_term()), disc_factors())
-pv_expenses() = dot(expenses.(0:12policies_term()), disc_factors())
 pv_net_cf() = pv_premiums() .- pv_claims() .- pv_expenses() .- pv_commissions()
-pv_pols_if() = dot(policies_inforce.(0:12policies_term()), disc_factors())
-pv_premiums() = dot(premiums.(0:12policies_term()), disc_factors())
-
-
-
 
 net_cf(t::Int) = premiums(t) .- claims(t) .- expenses(t) .- commissions(t)
-
 
 function result_cf()
     t_len = 0:12*policies_term()
--- test/callthings.jl
+++ test/callthings.jl
@@ -1,14 +1,17 @@
 using CacheFlow
+using CacheFlow: Gpt as gpt
 using Folds
-const gpt = CacheFlow.Gpt
+using Test
 
-@assert gpt.policies_inforce(200)[1:3] == [0.000000, 0.5724017900070532, 0.000000]
-@assert gpt.claims(130)[1:3] ≈ [0.0, 28.82531005791726, 0.0]
-@assert gpt.expenses(100)[1:3] == [3.682616858501336, 3.703818110341339, 3.671941182132007]
-@assert gpt.expenses(0)[1:3] == [305.0,305.0,305.0]
+@test gpt.policies_inforce(200)[1:3] == [0.000000, 0.5724017900070532, 0.000000]
+@test gpt.claims(130)[1:3] ≈ [0.0, 28.82531005791726, 0.0]
+@test gpt.expenses(100)[1:3] == [3.682616858501336, 3.703818110341339, 3.671941182132007]
+@test gpt.expenses(0)[1:3] == [305.0,305.0,305.0]
 
 # calls to `pv_` prefixed functions appear to be expensive. These functions are sometimes called at each timestep.
 # pv_claims works, but it is used in the calculation for premiums(t::Int) which can be called thousands of times potentially.
 # See implementation to see why operation is expensive.
-@assert gpt.pv_claims()[1:3] == [5501.19489836432, 5956.471604652321, 9190.425784230943]
+@test gpt.pv_claims()[1:3] ≈ [5501.19489836432, 5956.471604652321, 9190.425784230943]
+@test gpt.premiums(2)[1:3] ≈ [93.178897, 60.072723, 155.866742]
 
+# res = gpt.result_pv()
