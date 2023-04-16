870e57cef1bd586cc506966ae5d22dd1b6199b9e
MatthewCaseres <matthewcaseres@outlook.com>
2023-04-15 21:44:54 -0500

    more testing, implement pv_claims

--- src/gpt.jl
+++ src/gpt.jl
@@ -1,8 +1,8 @@
 module Gpt
-using DataFrames, CSV, LinearAlgebra, Statistics
+using DataFrames, CSV, LinearAlgebra, Statistics, Folds, Memoize
 
 mort_df = Matrix{Float64}(CSV.read(joinpath(@__DIR__, "models", "mort_table.csv"), DataFrame)[:, 2:end])
-# const model_points = CSV.read(joinpath(@__DIR__, "models", "model_point_table.csv"), DataFrame)
+const model_points = CSV.read(joinpath(@__DIR__, "models", "model_point_table.csv"), DataFrame)
 const issue_age = model_points[:, :age_at_entry]
 Base.@kwdef struct BasicMortality
     rates::Matrix{Float64} = Matrix{Float64}(CSV.read(joinpath(@__DIR__, "models", "mort_table.csv"), DataFrame)[:, 2:end])
@@ -34,8 +34,9 @@ function policies_maturity(t)
     (t .== 12policies_term()) .* (policies_inforce(t - 1) .- policies_lapse(t - 1) .- policies_death(t - 1))
   end
 
-const sum_assured = model_points[:, :sum_assured][1]
-const sex = model_points[:, :sex][1]
+const sum_assured = model_points[:, :sum_assured]
+const zero_spot = CSV.read("src/models/disc_rate_ann.csv", DataFrame)[:, :zero_spot]
+const sex = model_points[:, :sex]
 const inflation_rate = 0.01
 const expense_acq = 300
 const expense_maint = 60
@@ -44,7 +45,7 @@ const projection_length = 20 * 12
 
 age(t::Int) = age_at_entry() .+ duration(t)
 age_at_entry() = model_points[:, :age_at_entry]
-claim_pp(t::Int) = sum_assured
+claim_pp(t::Int) = sum_assured #unused
 claims(t::Int) = claim_pp(t) .* policies_death(t)
 commissions(t::Int) = duration(t) == 0 ? premiums(t) : 0
 disc_factors() = [(1 + disc_rate_mth(t))^(-t) for t in 0:12*policies_term]
@@ -52,17 +53,25 @@ disc_rate_mth(t::Int)::Float64 = (1 + disc_rate_ann(duration(t)))^(1/12) - 1
 disc_rate_ann(t::Int)::Float64 = 0.05 # where does that come from? t is unused
 expenses(t::Int) = t == 0 ? expense_acq .+ (policies_inforce(t) .* (expense_maint / 12) .* inflation_factor(t)) : (policies_inforce(t) .* (expense_maint / 12) .* inflation_factor(t))
 inflation_factor(t::Int) = (1 .+ inflation_rate).^(t/12)
-net_cf(t::Int) = premiums(t) .- claims(t) .- expenses(t) .- commissions(t)
+# this is tested but performance concerns.
+disc_factor(t) = (1 + zero_spot[duration(t)+1])^(-t/12)
+pv_claims() = Folds.sum(t -> claims(t) * disc_factor(t), 0:500)
 
+# See that net_premium_pp() references pv_claims, an expensive function. This gets called for every t during premiums(t::Int).
 net_premium_pp() = pv_claims() ./ pv_pols_if()
 premium_pp() = round.((1 .+ loading_prem) .* net_premium_pp(), 2)
 premiums(t::Int) = premium_pp() .* policies_inforce(t)
-pv_claims() = dot(claims.(0:12policies_term()), disc_factors())
 pv_commissions() = dot(commissions.(0:12*policies_term()), disc_factors())
 pv_expenses() = dot(expenses.(0:12policies_term()), disc_factors())
 pv_net_cf() = pv_premiums() .- pv_claims() .- pv_expenses() .- pv_commissions()
 pv_pols_if() = dot(policies_inforce.(0:12policies_term()), disc_factors())
 pv_premiums() = dot(premiums.(0:12policies_term()), disc_factors())
+
+
+
+
+net_cf(t::Int) = premiums(t) .- claims(t) .- expenses(t) .- commissions(t)
+
 
 function result_cf()
     t_len = 0:12*policies_term()
--- test/callthings.jl
+++ test/callthings.jl
@@ -1,68 +1,14 @@
 using CacheFlow
+using Folds
 const gpt = CacheFlow.Gpt
-const mbt = CacheFlow.MemoizedBasicTerm
 
-# we are checking for consistency between the functions in the modules
-@assert gpt.monthly_basic_mortality(100) == mbt.monthly_basic_mortality(100)
-# now for .lapse_rate
-@assert gpt.lapse_rate(100) == mbt.lapse_rate(100)
-gpt.policies_maturity(120)
-mbt.policies_maturity(120)
-gpt.policies_inforce(1)
-mbt.policies_inforce(1)
-empty!(gpt.d)
-empty!(mbt.d)
-# .- policies_lapse(t - 1) .- policies_death(t - 1) .- policies_maturity(t)
-# check for these 3 functions on timestep 1 the differences
-sum(gpt.policies_lapse(1))
-sum(mbt.policies_lapse(1))
-sum(gpt.policies_death(1))
-sum(mbt.policies_death(1))
-sum(gpt.policies_maturity(1))
-sum(mbt.policies_maturity(1))
-sum(gpt.policies_inforce(1))
-sum(mbt.policies_inforce(1))
-sum(gpt.policies_maturity(2))
-sum(mbt.policies_maturity(2))
-sum(gpt.policies_inforce(2))
-sum(mbt.policies_inforce(2))
-
-
-mbt = CacheFlow.MemoizedBasicTerm
-minimum(gpt.model_points[:, :policy_term])
-b = mbt.BasicMortality()
-mbt.monthly_basic_mortality(0)
-mbt.policies_inforce(100)
-# CacheFlow.UnmemoizedBasicTerm.policies_inforce(1)
-# get the length of a dataframe in Julia : length(df)
-CacheFlow.UnmemoizedBasicTerm.policies_inforce(3)
-CacheFlow.MemoizedBasicTerm.get_monthly_rate(10)
-CacheFlow.MemoizedBasicTerm.get_annual_rate(10)
-CacheFlow.MemoizedBasicTerm.annual_basic_mortality(1)
-CacheFlow.MemoizedBasicTerm.lapse_rate(10)
-CacheFlow.MemoizedBasicTerm.model_points
-
-mbt.policies_inforce(1)
-mbt.d
-mbt.graph
-empty!(mbt.graph)
-empty!(mbt.d)
-dd = de
-# clear the dictionary
-
-mbt.d
-
-using DataStructures
-
-d = DefaultDict([])
-push!(d["1"], 2)
-d
-
-
-mbt.policies_inforce(200)[1:3]
-# assert that the first three entries of  CacheFlow.MemoizedBasicTerm.policies_inforce(200) are good
-@assert CacheFlow.MemoizedBasicTerm.policies_inforce(200)[1:3] == [0.000000, 0.5724017900070532, 0.000000]
 @assert gpt.policies_inforce(200)[1:3] == [0.000000, 0.5724017900070532, 0.000000]
-gpt.pv_net_cf()
-gpt.premiums(1)
-gpt.net_cf(1)
+@assert gpt.claims(130)[1:3] ≈ [0.0, 28.82531005791726, 0.0]
+@assert gpt.expenses(100)[1:3] == [3.682616858501336, 3.703818110341339, 3.671941182132007]
+@assert gpt.expenses(0)[1:3] == [305.0,305.0,305.0]
+
+# calls to `pv_` prefixed functions appear to be expensive. These functions are sometimes called at each timestep.
+# pv_claims works, but it is used in the calculation for premiums(t::Int) which can be called thousands of times potentially.
+# See implementation to see why operation is expensive.
+@assert gpt.pv_claims()[1:3] == [5501.19489836432, 5956.471604652321, 9190.425784230943]
+
